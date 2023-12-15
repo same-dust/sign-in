188d4e1054a95b733b5041d5d8bb12e5ed8ea3df
same-dust <2484193294@qq.com>
2023-12-15 19:30:55 +0800

    Update models.py

--- App/models.py
+++ App/models.py
@@ -80,7 +80,7 @@ class CourseTeacher(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     teacher=db.Column(db.String(30))
     course=db.Column(db.String(42))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=True)
 
 class TimePlace(db.Model):
     __tablename__ = 'timeplace'
